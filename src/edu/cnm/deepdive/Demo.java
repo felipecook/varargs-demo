96252e45890362ee360b0d3cff8d079ef9dd0deb
Felipe Cook <felipepcook@gmail.com>
2019-02-11 08:45:39 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Demo.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive;
+
+import java.util.Arrays;
+
+public class Demo {
+
+  public static void doSomething(int[] data) {
+    System.out.println(Arrays.toString(data));
+  }
+
+  public static void main(String... args){
+    
+  }
+}
